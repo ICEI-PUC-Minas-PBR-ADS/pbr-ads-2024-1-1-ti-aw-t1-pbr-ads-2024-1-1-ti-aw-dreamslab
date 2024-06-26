4ea11019a5c46706d0f9bc3020873d5c1e2da053
DanielFilipe100 <daniel.filipesoarescv@gmail.com>
2024-06-26 14:04:01 -0300

    correcoes css

--- src/assets/scripts/empresa/home.js
+++ src/assets/scripts/empresa/home.js
@@ -11,12 +11,40 @@ $(function(){
         return;
     }
 
+    $(".links-right").append('<div class="logout-div"><h1 class="logout">Logout</h1></div>');
+    $(".logout").on("click", function(){
+        localStorage.clear();
+        location.reload();
+    })
     setHomeData();
 })
 
 function setHomeData(){
     $(".load").remove();
-    new BusinessController().getBusinessDataByOwnerId(result.userId).then((businessData)=>{
-    console.log(businessData);
-})
+    // const userId = localStorage.getItem("userId");
+    //     new BusinessController().getBusinessDataByOwnerId(userId).then((businessData)=>{
+    //     console.log(businessData);
+    // })
 }
+
+
+// $(function(){
+//     if(localStorage.getItem("isBusiness") != null){
+//         window.location = "../src/empresa/home.html";
+//         return;
+//     }
+
+//     if(localStorage.getItem("userId") == null){
+//         window.location = "./login.html";
+//         return;
+//     }
+    
+//     $(".links-right").append('<div class="logout-div"><h1 class="logout">Logout</h1></div>');
+//     $(".logout").on("click", function(){
+//         console.log("teste")
+//         localStorage.clear();
+//         location.reload();
+//     })
+//     setHomeData();
+//     setCart();
+// })
